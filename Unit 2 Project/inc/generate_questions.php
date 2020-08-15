9e121a9078a89960be4e154bcb94c45927c14ac4
FelixA-GitHub <67863506+FelixA-GitHub@users.noreply.github.com>
2020-08-15 12:44:56 -0400

    Trying to get correct answers
    When clicking on correct answers, they are coming up incorrect.

--- Unit 2 Project/inc/generate_questions.php
+++ Unit 2 Project/inc/generate_questions.php
@@ -1,13 +1,31 @@
 <?php
 // Generate random questions
 
-// Loop for required number of questions
+$questions = [];
 
-// Get random numbers to add
+// Loop for required number of questions
+for ($i = 0; $i <= 9; $i++) {
+
+  // Get random numbers to add
+  $questions[$i]['leftAdder'] = rand(0,50);
+  $questions[$i]['rightAdder'] = rand(0,50);
 
 // Calculate correct answer
+  $questions[$i]['correctAnswer'] = ($questions[$i]['leftAdder']) + ($questions[$i]['rightAdder']);
 
 // Get incorrect answers within 10 numbers either way of correct answer
+  $questions[$i]['firstIncorrectAnswer'] = $questions[$i]['correctAnswer'] + rand(-10,10);
+  $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] + rand(-10,10);
+
 // Make sure it is a unique answer
+  do {
+        $questions[$i]['firstIncorrectAnswer'] = $questions[$i]['correctAnswer'] + rand(-10,10);
+    } while ($questions[$i]['firstIncorrectAnswer'] == $questions[$i]['correctAnswer']);
+
+  do {
+        $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] + rand(-10,10);
+    } while ($questions[$i]['secondIncorrectAnswer'] == $questions[$i]['correctAnswer']);
 
 // Add question and answer to questions array
+}
+//echo $questions;
